b85ecb7c22f23e0de82bd95756fbda2b2f2e5f23
Aryeh Naar <naar@student.uts.edu.au>
2022-05-16 21:04:06 +1000

    Began seat model

--- Assignment_3_IOS/models/seats.swift
+++ Assignment_3_IOS/models/seats.swift
@@ -9,4 +9,14 @@ import UIKit
 
 class Seats: UIButton {
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+    
+    
 }
