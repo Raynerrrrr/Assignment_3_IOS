d271ff0abfa60ee88a7abefbea9de0a26e7add22
Aryeh Naar <naar@student.uts.edu.au>
2022-05-18 14:36:32 +1000

    seat selection

--- Assignment_3_IOS/controllers/SeatsViewController.swift
+++ Assignment_3_IOS/controllers/SeatsViewController.swift
@@ -7,44 +7,126 @@
 
 import UIKit
 
-class SeatsViewController: UIViewController
+class SeatsViewController: UIViewController, SeatSelectorDelegate
 {
-    var seats = Seats()
-    var seatSelection: String = ""
-    var screenWidth: UInt32 = UInt32(UIScreen.main.bounds.width)
-    var screenHeight: UInt32 = UInt32(UIScreen.main.bounds.height)
+    var userSeats: [String] = []
+    var seatData: String = ""
+    var userSeatsCount: Int = 0
     
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-//        displaySeats()
+        // Do any additional setup after loading the view, typically from a nib.
+        let seatMap: String =   "_AA_AAAAA_AA_/" +
+                            "_AA_AAAAA_AA_/" +
+                            "_AA_AAAAA_AA_/" +
+                            "_AA_AAAAA_AA_/" +
+                            "_AA_AAAAA_AA_/" +
+                            "_AA_AAAAA_AA_/" +
+                            "_AA_AAAAA_AA_/" +
+                            "_AA_AAAAA_AA_/"
+        
+        let seats = SeatSelector()
+        seats.frame = CGRect(x: 0, y: 250, width: self.view.frame.size.width, height: 600)
+        seats.setSeatSize(CGSize(width: 30, height: 30))
+        seats.setAvailableImage(   UIImage(named: "A")!,
+            andUnavailableImage:    UIImage(named: "U")!,
+            andDisabledImage:       UIImage(named: "D")!,
+            andSelectedImage:       UIImage(named: "S")!)
+        seats.layout_type = "Normal"
+        seats.setMap(seatMap)
+        seats.selected_seat_limit  = 5
+        seats.seatSelectorDelegate = self
+        seats.maximumZoomScale         = 5.0
+        seats.minimumZoomScale         = 0.05
+        self.view.addSubview(seats)
+        
+        
     }
     
-//    @objc func displaySeats() {
-//        let numberOfSeats = 20
-//        var i = 0
-//
-//        var x = 100
-//        var y = screenHeight / 2
-//
-//
-//        while i < numberOfSeats {
-//            seats = Seats()
-//            seats.frame = CGRect(x: x, y: Int(y), width: 100, height: 100)
-//            x += 15
-//            if i == 5 || i == 10 || i == 15 || i == 20 {
-//                y += 20
-//            }
-//            i += 1
-//        }
-//
-//    }
+    func seatSelected(_ seat: Seat) {
+//        print("Seat at row: \(seat.row) and column: \(seat.column)\n")
+        let seatNumber = String(seat.column - 1)
+        var seatRow = ""
+        switch seat.row {
+        case 1:
+            seatRow = "A"
+        case 2:
+            seatRow = "B"
+        case 3:
+            seatRow = "C"
+        case 4:
+            seatRow = "D"
+        case 5:
+            seatRow = "E"
+        case 6:
+            seatRow = "F"
+        case 7:
+            seatRow = "G"
+        case 8:
+            seatRow = "H"
+        default:
+            seatRow = ""
+        }
+        
+        seatData = seatRow + seatNumber
+        if seat.selected_seat {
+            userSeats.append(seatData)
+            print(userSeats)
+        } else {
+                for i in 0...userSeats.count - 1 {
+                    if userSeats[i] == seatData {
+                        userSeats.remove(at: i)
+                    }
+                }
+            print(userSeats)
+        }
+    }
+    
+    func getSelectedSeats(_ seats: NSMutableArray) {
+        for i in 0..<seats.count {
+            let seat: Seat  = seats.object(at: i) as! Seat
+            let seatNumber = String(seat.column - 1)
+            var seatRow = ""
+            switch seat.row {
+            case 1:
+                seatRow = "A"
+            case 2:
+                seatRow = "B"
+            case 3:
+                seatRow = "C"
+            case 4:
+                seatRow = "D"
+            case 5:
+                seatRow = "E"
+            case 6:
+                seatRow = "F"
+            case 7:
+                seatRow = "G"
+            case 8:
+                seatRow = "H"
+            default:
+                seatRow = ""
+            }
+//            seatData = seatRow + seatNumber
+//            print(seatData)
+        }
+    }
+    
+    func saveSeatData() {
+        userSeats.removeAll()
+        userSeats.append(seatData)
+        print(userSeats)
+    }
     
     @IBAction func onConfirmClick(_ sender: Any)
     {
-        
-        UserDefaults.standard.set(seatSelection, forKey: MOVIE_SEAT)
-        self.performSegue(withIdentifier: "confirmSegue", sender: nil)
+//        saveSeatData()
+        userSeats.append(seatData)
+        print(userSeats)
+        if userSeats.isEmpty {
+            
+        } else {
+            //        self.performSegue(withIdentifier: "confirmSegue", sender: nil)
+        }
     }
 }
